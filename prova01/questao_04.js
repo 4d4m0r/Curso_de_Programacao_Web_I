709dd4138edfc75d2aa81ed4a59356b392143df3
4d4m0r <adamorbarbosa0@gmail.com>
2022-04-20 17:31:04 -0400

    ok

---
+++ prova01/questao_04.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+function acessaArq (arq) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(arq, function(error, dado) {
+            resolve(parseInt(dado));
+        });
+    });
+}
+
+async function calcularValor () {
+    var total = 0;
+    total += await acessaArq("1.txt");
+    total += await acessaArq("2.txt");
+    total += await acessaArq("3.txt");
+    console.log(total);
+}
+
+calcularValor();
